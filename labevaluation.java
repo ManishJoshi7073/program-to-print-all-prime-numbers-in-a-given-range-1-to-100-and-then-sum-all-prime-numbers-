3980106a8b83bb99f1ed681c4d72d4629f5edec7
ManishJoshi7073 <49111638+ManishJoshi7073@users.noreply.github.com>
2020-01-24 15:44:50 +0530

    Add files via upload

---
+++ labevaluation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner; 
+class labevaluation
+{
+public static void main(String[] args) {
+int i,j,isprime=0,sum=0;
+System.out.println("Prime Number for given range is: ");
+for(i=2;i<=100;i++)
+{
+	isprime=0;
+  for(j=2;j<=i/2;j++)
+  {
+     if(i%j==0)
+     {
+     	isprime=1;
+     	break;
+     	 }
+
+  }
+
+  if(isprime==0)
+  {
+     System.out.println(" "+i);
+     sum+=i;
+  }
+}
+System.out.println("Sum of the numbers are "+sum);
+}
+}
